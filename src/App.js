cc61fdd2fe0b7ed99ee191e0dfeea336dc712e86
Gino Notto <gnotto@bloomberg.net>
2018-06-13 10:28:26 -0400

    hehe

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ class App extends Component {
     return (
       <div className="App">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossOrigin="anonymous"/>
+        {Date(Date.now()) >= new Date('06/14/2018') && new Date(Date.now()) <= new Date('06/15/2018') ?
+          <div className="happy-birthday">
+            <div className="bday-pic">
+              <img src="/happy_birthday.png"/>
+            </div>
+            <div className="bday-msg">
+              <span className="filler"/>
+              <h1> Happy Birthday, Racheal! </h1>
+              <span className="filler"/>
+            </div>
+          </div>
+        : null}
         <h1> Guest List So Far </h1>
         <RSVPListGetter/>
       </div>
